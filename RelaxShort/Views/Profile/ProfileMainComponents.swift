665f0a018beddd205b492131f6ce6cd7537e0adf
Ethan <ethan@relaxshort.local>
2026-07-05 17:34:49 +0800

    style: coin pulse 1.5x @ 0.5s

--- RelaxShort/Views/Profile/ProfileMainComponents.swift
+++ RelaxShort/Views/Profile/ProfileMainComponents.swift
@@ -402,9 +402,9 @@ struct ProfileMenuRow: View {
                         Image(systemName: sIcon)
                             .font(.system(size: 14))
                             .foregroundColor(subtitleIconColor)
-                            .scaleEffect(coinPulse ? 1.35 : 1.0)
+                            .scaleEffect(coinPulse ? 1.5 : 1.0)
                             .onAppear {
-                                withAnimation(.easeInOut(duration: 0.6).repeatForever(autoreverses: true)) {
+                                withAnimation(.easeInOut(duration: 0.5).repeatForever(autoreverses: true)) {
                                     coinPulse = true
                                 }
                             }
